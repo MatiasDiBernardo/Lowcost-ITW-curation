7ae5ce7d957bb02510ee6c4c67d4b7441916ec3c
nacho-correa <n.correa.213@gmail.com>
2026-02-06 21:37:35 -0300

    Refactor reset_conditions.py: Update audio recovery logic to dynamically list audios and improve folder cleanup process

--- Utils/reset_conditions.py
+++ Utils/reset_conditions.py
@@ -1,35 +1,27 @@
 import os
 import shutil
 
-
-# To use when you want to return to the original state (here put the audios you want to test with)
-audios_to_recover = ["Test1.wav", "Test2.wav", "Test3.wav", "Test4.wav", "Test5.wav", "Test6.wav", "Test7.wav"]
-
-
 # Returns to the process folder
-process_path = os.path.join("Datos", "Audio_to_Process")
-raw_path = os.path.join("Datos", "Audios_Raw")
+process_path = os.path.join("Data", "Audio_to_Process")
+raw_path = os.path.join("Data", "Audios_Raw")
+
+audios_to_recover = os.listdir(raw_path)
 for audio in audios_to_recover:
     shutil.move(os.path.join(raw_path, audio), os.path.join(process_path, audio))
 
-
-# Removes audios in denoised
-denoise_path = os.path.join("Datos", "Audios_Denoise") 
-for audio in audios_to_recover:
-    path_to_remove = os.path.join(denoise_path, audio)
-    if os.path.exists(path_to_remove):
-        os.remove(path_to_remove)
-
-
 # Removes folders in vad, clean and transcript
-vad_path = os.path.join("Datos", "Audios_VAD") 
-clean_audios_path = os.path.join("Datos", "Audios_Clean") 
-transcript_path = os.path.join("Datos", "Audios_Transcript") 
+vad_path = os.path.join("Data", "Audios_VAD") 
+denoise_path = os.path.join("Data", "Audios_Denoise") 
+speaker_filt_path = os.path.join("Data", "Audios_Speaker_Filt")
+clean_audios_path = os.path.join("Data", "Audios_Clean") 
+transcript_path = os.path.join("Data", "Audios_Transcript") 
 
 name_folders = [name.split(".")[0] for name in audios_to_recover]
 
 for folder in name_folders:
     shutil.rmtree(os.path.join(vad_path, folder), ignore_errors=True)
+    shutil.rmtree(os.path.join(denoise_path, folder), ignore_errors=True)
+    shutil.rmtree(os.path.join(speaker_filt_path, folder), ignore_errors=True)
     shutil.rmtree(os.path.join(clean_audios_path, folder), ignore_errors=True)
     shutil.rmtree(os.path.join(transcript_path, folder), ignore_errors=True)
     
